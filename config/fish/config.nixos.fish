c5f806fba580945babffe8ac6c5e65b99c739864
Oleg Tverdyy Tereshchenko <alexverdyy@gmail.com>
2025-09-21 20:03:15 +0200

    update submodules

--- config/fish/config.nixos.fish
+++
@@ -1,7 +0,0 @@
-alias logout 'sudo pkill -KILL -u tverdyy'
-alias nix-files 'cd ~/nixos-dotfiles && nvim'
-alias nrs 'sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos'
-alias btw "echo i use nixos btw"
-
-# Projects directories
-set -gx PROJECT_PATHS ~/Projects
